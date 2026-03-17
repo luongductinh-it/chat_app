c6e0dcfe966f22b39b94f932a5dd08678c0a287b
luongductinh-it <ldt20062003@gmail.com>
2026-03-17 22:28:56 +0700

    chore: add package.json for Firebase Cloud Functions setup to send notifications automatically

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require("firebase-functions/v2/firestore");
+const admin = require("firebase-admin");
+
+
+admin.initializeApp();
+exports.myFunction = functions.onDocumentCreated(
+  "chat/{messageId}",
+  (event) => {
+    const data = event.data.data();
+    return admin.messaging().send({
+      notification: {
+        title: data["userName"],
+        body: data["text"],
+      },
+      data: {
+        click_action: "FLUTTER_NOTIFICATION_CLICK",
+      },
+      topic: "chat",
+    });
+  }
+);
